8340f742545a1b6cc9b355b51d7f75e5ab30945a
Jialiang Chen <jialiang.chen@estudiants.urv.cat>
2021-10-19 11:53:02 +0200

    prog1: avances en 1B

--- source/candy1_init.s
+++ source/candy1_init.s
@@ -148,20 +148,22 @@ inicializa_matriz:
 @;	r4= (i*COLUMNS)+j
 @;	r5= #COLUMNS
 @;	r6= valor actual de matriz[i][j]
-@;	r7= 
-@;	r8= 
-@;	r9= 
-@;	r10= 
-@;	r11= 
+@;	r7= mat_recomb1[i][j]
+@;	r8= mat_recomb2[i][j]
+@;	r9= temporal (valor actual de mat_recomb1[i][j]
+@;	r10= temporal (valor actual de mat_recomb2[i][j]
+@;	r11= temporal ()
 @;	r12= Copia de la dirección base de la matriz de juego
 @;	PARÁMETROS: R0 = dirección base de la matriz de juego
 	.global recombina_elementos
 recombina_elementos:
 		push {lr}
-		mov 12, r0				@;Backup de dirección base
+		mov 12, r0				@;Backup de dirección base (@matriz[0][0])
 		mov r1, #0				@;Inicializar i
 		mov r2, #0				@;Inicializar j 
 		mov r5, #COLUMNS		@;r5=#COLUMNS
+		ldr r7, =mat_recomb1	@;r7=@mat_recomb1[0][0]
+		ldr r8, =mat_recomb2	@;r8=@mat_recomb2[0][0]
 	.Lfor1:
 		cmp r1, #ROWS			@;comprovar que no s'ha sortit de la taula
 		bhs .Lendfor1			@;saltar si ja ha recorregut totes les files
@@ -169,12 +171,19 @@ recombina_elementos:
 	.Lfor2:
 		cmp r2, #COLUMNS		@;comprovar que no s'ha sortit de la taula
 		bhs .Lendfor2			@;saltar si ja ha recorregut totes les columnes
-		mla r4, r1, r5, r2		@; r4 = (i*COLUMNS)+j
-		ldrb r6, [r12, r4]
+		mla r4, r1, r5, r2		@;r4 = (i*COLUMNS)+j
+		ldrb r6, [r12, r4]		@;r6 = matriz[i][j]
+		ldrb r9, [r7, r4]		@;r9 = mat_recomb1[i][j]
 	@; IF1
-		
-		
-		
+		orr r6, #0				@;porta or de matriz[i][j] amb #0
+		cmp r6, #7				@;detectar si és 111
+		bne .Lelse				@;saltar si té els últims 3 bits a 1
+		cmp r6, #0				@;detectar si és 000
+		bne .Lelse				@;saltar si té els últims 3 bits a 0
+	.Lelse1:
+	
+	
+	
 		
 		
 		
